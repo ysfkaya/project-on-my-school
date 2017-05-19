aa26eae21f4bebbae93b3faa4272d3596d2cd622
Yusuf Kaya <yusuf.kaya.x0@gmail.com>
2017-05-20 01:26:37 +0300

    add config file

---
+++ system/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Veritabanı hostname'i
+$_host = 'localhost';
+
+// Veritabanı kullanıcı adı
+$_username = 'root';
+
+// Veritabanı şifresi
+$_password = 'root';
+
+// Veritabanı adı
+$_dbname = 'proje_takip';
